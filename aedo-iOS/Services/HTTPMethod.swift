faa93dc5c8268c90fce91dde8c6f1081a9233734
minseongkim97 <kyrok67@naver.com>
2022-03-31 20:21:17 +0900

    feat(splash): add policyService and save all app policy

---
+++ aedo-iOS/Services/HTTPMethod.swift
@@ -0,0 +1,15 @@
+//
+//  HTTPMethod.swift
+//  aedo-iOS
+//
+//  Created by MIN SEONG KIM on 2022/03/31.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case GET
+    case POST
+    case PUT
+    case DELETE
+}
--- aedo-iOS/Views/Controllers/SplashViewController.swift
+++ aedo-iOS/Views/Controllers/SplashViewController.swift
@@ -12,7 +12,8 @@ class SplashViewController: UIViewController {
     //MARK: - Properties
     // Realm은 싱글톤 객체를 생성한다.
     let realm = try! Realm()
-    let verificationDataService = VerificationDataService()
+    let verificationService = VerificationService()
+    let policyService = PolicyService()
     
     //MARK: - Lifecycle
     override func viewDidLoad() {
@@ -37,6 +38,60 @@ class SplashViewController: UIViewController {
     //MARK: - Actions
     
     //MARK: - Helpers
+    private func writeAppPolicy(_ allPolicy: AllPolicy) {
+        let appPolicy = AppPolicy()
+        appPolicy._id = allPolicy.id
+        appPolicy.result = allPolicy.result
+        
+        allPolicy.policy.forEach { polictEntity in
+            let policy = Policy()
+            policy.id = polictEntity.id
+            policy.name = polictEntity.name
+            policy.value = polictEntity.value
+            appPolicy.policy.append(policy)
+        }
+        
+        allPolicy.code.forEach { codeEntity in
+            let code = Code()
+            code.cd = codeEntity.cd
+            code.name = codeEntity.name
+            code.name_en = codeEntity.nameEn
+            code.category_cd = codeEntity.categoryCd
+            code.category_name = codeEntity.categoryName
+            appPolicy.code.append(code)
+        }
+        
+        allPolicy.appMenu.forEach { appMenuEntity in
+            let appMenu = AppMenu()
+            appMenu.cd = appMenuEntity.cd
+            appMenu.name = appMenuEntity.name
+            appMenu.name_en = appMenuEntity.nameEn
+            appMenu.category_cd = appMenuEntity.categoryCd
+            appMenu.category_name = appMenuEntity.categoryName
+            appMenu.icon_name = appMenuEntity.iconName
+            appMenu.use_yn = appMenuEntity.useYN
+            appPolicy.app_menu.append(appMenu)
+        }
+        
+        allPolicy.coordinates.forEach({ coordinateEntity in
+            let coordinates = Coordinate()
+            coordinates.id = coordinateEntity.id
+            coordinates.name = coordinateEntity.name
+            coordinates.xvalue = coordinateEntity.xvalue
+            coordinates.yvalue = coordinateEntity.yvalue
+            appPolicy.coordinates.append(coordinates)
+        })
+        
+        do {
+            try realm.write {
+                realm.add(appPolicy)
+            }
+        } catch {
+            print("realm write error")
+            showSystemMaintenanceAlert()
+        }
+    }
+    
     private func writeAppVerification(_ appVerfication: Verification) {
         let verification = AppVerification()
         verification.result = appVerfication.result
@@ -73,11 +128,28 @@ class SplashViewController: UIViewController {
     }
     
     private func managerVerficationData() {
-        verificationDataService.getVerification { [weak self] result in
+        verificationService.getVerification { [weak self] result in
             switch result {
             case .success(let verification):
                 DispatchQueue.main.async {
                     self?.writeAppVerification(verification)
+                }
+                
+            case .failure(.invalidURL), .failure(.unableToComplete), .failure(.invalidResponse), .failure(.invalidData):
+                print("get verfication data is failed")
+                DispatchQueue.main.async {
+                    self?.showSystemMaintenanceAlert()
+                }
+            }
+        }
+    }
+    
+    private func managerPolicyData() {
+        policyService.getPolicy { [weak self] result in
+            switch result {
+            case .success(let policy):
+                DispatchQueue.main.async {
+                    self?.writeAppPolicy(policy)
                 }
                 
             case .failure(.invalidURL), .failure(.unableToComplete), .failure(.invalidResponse), .failure(.invalidData):
