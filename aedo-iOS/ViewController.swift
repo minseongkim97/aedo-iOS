a21858ed04e0c249c1cda6ed330f6f23ba8fb004
minseongkim97 <kyrok67@naver.com>
2022-03-31 12:04:46 +0900

    Initial Commit

---
+++ aedo-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  aedo-iOS
+//
+//  Created by MIN SEONG KIM on 2022/03/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
