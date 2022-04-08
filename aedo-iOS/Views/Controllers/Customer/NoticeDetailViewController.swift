287ef91036fef601a34067f3c591c223e4fa6146
minseongkim97 <kyrok67@naver.com>
2022-04-08 12:26:06 +0900

    design(notice): make notice detail view controller

---
+++ aedo-iOS/Views/Controllers/Customer/NoticeDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NoticeDetailViewController.swift
+//  aedo-iOS
+//
+//  Created by MIN SEONG KIM on 2022/04/08.
+//
+
+import UIKit
+
+class NoticeDetailViewController: UIViewController {
+    //MARK: - Properties
+    
+    //MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
