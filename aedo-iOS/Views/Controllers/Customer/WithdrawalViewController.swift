da1eafddc20fa9609ab19f3b82cd20158f622fe1
minseongkim97 <kyrok67@naver.com>
2022-04-15 16:19:56 +0900

    design(Withdrawl): make WithdrawlVC UI

--- aedo-iOS/Views/Controllers/Customer/WithdrawalViewController.swift
+++ aedo-iOS/Views/Controllers/Customer/WithdrawalViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 class WithdrawalViewController: UIViewController {
     //MARK: - Properties
     static let identifier = "WithdrawalViewController"
+    @IBOutlet private weak var noticeView: UIView!
     
     //MARK: - Lifecycles
     override func viewDidLoad() {
         super.viewDidLoad()
+        setNoticeView()
+    }
+    
+    //MARK: - Helpers
+    private func setNoticeView() {
+        noticeView.layer.borderWidth = 1
+        noticeView.layer.borderColor = UIColor(hex: 0xDDDDDD).cgColor
+        noticeView.layer.cornerRadius = 10
     }
 }
