af64874ecd3133720bbc9e3d58967915d63c24bc
minseongkim97 <kyrok67@naver.com>
2022-04-15 17:07:10 +0900

    design(DetailInquiry): make DetailInquiryVC UI

--- aedo-iOS/Views/Controllers/Customer/DetailInquiryViewController.swift
+++ aedo-iOS/Views/Controllers/Customer/DetailInquiryViewController.swift
@@ -10,8 +10,18 @@ import UIKit
 class DetailInquiryViewController: UIViewController {
     //MARK: - Properties
     static let identifier = "DetailInquiryViewController"
+    @IBOutlet private weak var answerView: UIView!
+    
     //MARK: - Lifecycles
     override func viewDidLoad() {
         super.viewDidLoad()
+        setAnswerView()
+    }
+    
+    //MARK: - Helpers
+    private func setAnswerView() {
+        answerView.layer.borderWidth = 1
+        answerView.layer.borderColor = UIColor(hex: 0xDDDDDD).cgColor
+        answerView.layer.cornerRadius = 10
     }
 }
