356d4cd766edef522faad28ba9e1e33d0f4c35c2
minseongkim97 <kyrok67@naver.com>
2022-04-17 19:53:49 +0900

    design(Obituary): make ObituaryListVC UI

---
+++ aedo-iOS/Views/Controllers/ObituaryList/ObituaryListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ObituaryListTableViewCell.swift
+//  aedo-iOS
+//
+//  Created by MIN SEONG KIM on 2022/04/17.
+//
+
+import UIKit
+
+class ObituaryListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ aedo-iOS/Views/Controllers/ObituaryList/ObituaryListViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ObituaryListViewController.swift
+//  aedo-iOS
+//
+//  Created by MIN SEONG KIM on 2022/04/17.
+//
+
+import Foundation
